2ff4c24d026eeb5bf34b31a8ea5995a711ecf267
sketchy-ota <oliver@sketchy.com>
2020-02-16 15:14:48 -0800

    Update cart to be an array

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -2,19 +2,24 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class CartService {
-  items = {}
+  items = [] // {}
 
   constructor() { }
 
   addToCart(product) {
-    this.items[product]
-      ? this.items[product]++
-      : this.items[product] = 1
+    // this.items[product.name]
+    //   ? this.items[product.name]++
+    //   : this.items[product.name] = 1
+    this.items.push(product)
   }
 
   removeFromCart(product) {
-    this.items[product] > 0
-      ? this.items[product]--
+    // this.items[product.name] > 0
+    //   ? this.items[product.name]--
+    //   : window.alert("Not in Cart")
+    let itemIdx = this.items.indexOf(product)
+      itemIdx > -1
+      ? this.items = this.items.filter(itemIdx)
       : window.alert("Not in Cart")
   }
 
