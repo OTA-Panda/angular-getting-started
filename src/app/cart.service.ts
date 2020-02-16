f9ff281761cd6edd5f7dbddc759758295b2ce10b
sketchy-ota <oliver@sketchy.com>
2020-02-16 13:57:18 -0800

    Define Cart Service Functions

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -2,7 +2,26 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class CartService {
+  items = {}
 
   constructor() { }
 
+  addToCart(product) {
+    this.items[product] = 1
+  }
+
+  removeFromCart(product) {
+    this.items[product] > 0
+      ? this.items[product]--
+      : window.alert("Not in Cart")
+  }
+
+  getItems() {
+    return this.items
+  }
+
+  clearCart() {
+    this.items = {}
+    return this.items
+  }
 }
